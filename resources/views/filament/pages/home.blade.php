af21e0a4aa28fb0856d34c27fc7aab5a8e227528
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-09-19 16:59:35 -0300

    Incluindo o selo

--- resources/views/filament/pages/home.blade.php
+++ resources/views/filament/pages/home.blade.php
@@ -4,8 +4,16 @@
             background-image: url('img/standard-quality-control-concept-m.png');
             background-size: 100%;
         }
+        .image-container {
+            text-align: center;
+            margin-top: 20px;
+        }
+        .image-container img {
+            margin-left: 40%;
+            width: 150px; /* Definindo um tamanho pequeno */
+            height: auto; /* Mantendo a proporção da imagem */
+        }
     </style>
-
 
     <x-filament::section>
         <x-slot name="heading">
@@ -14,17 +22,15 @@
 
         <x-filament-panels::form wire:submit="authenticate">
             {{ $this->form }}
-
         </x-filament-panels::form>
 
         <br>
         <hr>
         <br>
-        
+
         <nav
             class="fi-tabs flex max-w-full justify-center items-center text-center gap-x-1 overflow-x-auto mx-auto rounded-xl bg-white p-2 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10">
-            <x-filament::button wire:click="redirectToFormSelection" icon="heroicon-o-chat-bubble-left-right"
-                size="lg">
+            <x-filament::button wire:click="redirectToFormSelection" icon="heroicon-o-chat-bubble-left-right" size="lg">
                 COMUNICAÇÃO
             </x-filament::button>
 
@@ -38,16 +44,18 @@
                 POLÍTICA DE INTEGRIDADE
             </x-filament::button>
 
-            <x-filament::button wire:click="redirectToConsultation" icon="heroicon-o-document-magnifying-glass"
-                size="lg">
+            <x-filament::button wire:click="redirectToConsultation" icon="heroicon-o-document-magnifying-glass" size="lg">
                 CONSULTA
             </x-filament::button>
         </nav>
 
+        <!-- Adicionando a imagem pequena e centralizada -->
+        <div class="image-container">
+            <img src="{{ asset('img/selo.jpeg') }}" alt="Imagem descritiva">
+        </div>
+
         <x-slot name="headerEnd">
             {{-- Input to select the user's ID --}}
         </x-slot>
-
     </x-filament::section>
-
 </x-filament-panels::page.simple>
